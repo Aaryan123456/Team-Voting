78e0e9fb4f582249df77dc195f25a066263c0a8f
AaryanP <70894195+Aaryan123456@users.noreply.github.com>
2022-04-04 18:48:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,22 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7Ie4hIuV9ZqU3E5gZG1YXrbY6wEyH9ic",
+  authDomain: "votingapp-88598.firebaseapp.com",
+  databaseURL: "https://votingapp-88598-default-rtdb.firebaseio.com",
+  projectId: "votingapp-88598",
+  storageBucket: "votingapp-88598.appspot.com",
+  messagingSenderId: "726660856827",
+  appId: "1:726660856827:web:c7c4ddd735273db33fe95e"
 };
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 export default firebase.database();
